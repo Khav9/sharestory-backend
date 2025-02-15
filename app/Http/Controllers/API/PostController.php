0ce070a80084efb4ed9c4a249c268250db46af74
khav <khav.saroeun@gmail.com>
2025-02-15 19:33:07 +0700

    change on post controller

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,17 +7,20 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Throwable;
 
 class PostController extends Controller
 {
     public function index()
     {
         try {
-            $posts = Post::all()->map(function ($post) {
+            $posts = Post::where('publish', 1)->get()->map(function ($post) {
                 $post->cover = $post->cover ? Storage::url($post->cover) : null;
                 return $post;
             });
-    
+
             return response()->json([
                 'message' => 'Posts retrieved successfully',
                 'data' => $posts
@@ -29,5 +32,29 @@ class PostController extends Controller
             ], 500);
         }
     }
-    
+
+    public function show($id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+
+            return response()->json([
+                'success' => true,
+                'data' => $post,
+                'message' => 'Post retrieved successfully'
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 404);
+        } catch (Throwable $e) {
+            Log::error($e);
+
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while retrieving the post.'
+            ], 500);
+        }
+    }
 }
